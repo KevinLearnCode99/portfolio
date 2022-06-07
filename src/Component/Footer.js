a93d1a1ed04cc34f90b9af033ca3b9675e5f9fe5
KevinDuong1102 <phucthinh02.duong@gmail.com>
2022-06-06 22:30:09 -0600

    portfolio first version

---
+++ src/Component/Footer.js
@@ -0,0 +1,11 @@
+import "../Styles/FooterStyles.css"
+function Footer() {
+  return (
+    <div className="footer-wrapper">
+      <p>
+        Coded by <span>Kevin Duong</span>
+      </p>
+    </div>
+  );
+}
+export default Footer;
---
+++ src/data/ContactData.js
@@ -0,0 +1,18 @@
+export const ContactData = [
+  {
+    description: "email",
+    data: "phucthinh02.duong@gmail.com",
+  },
+  {
+    description: "github",
+    data: "https://github.com/KevinLearnCode99",
+  },
+  {
+    description: "phone#",
+    data: "+1587-664-2581",
+  },
+  {
+    description: "linkedin",
+    data: "https://www.linkedin.com/in/phuc-thinh-duong-6092061b7/",
+  },
+];
